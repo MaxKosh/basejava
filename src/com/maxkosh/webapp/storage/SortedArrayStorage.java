a8542278175ec2bb478080fd51d6cc1ad2767269
Max Koshelev <maxdiorio@yandex.ru>
2019-12-16 00:19:28 +0300

    HW3 (refactoring with arraycopy)

--- src/com/maxkosh/webapp/storage/SortedArrayStorage.java
+++ src/com/maxkosh/webapp/storage/SortedArrayStorage.java
@@ -19,21 +19,14 @@ public class SortedArrayStorage extends AbstractArrayStorage {
     @Override
     protected void insertResume(Resume resume, int index) {
         int insertIndex = (index + 1) * (-1);
-        size++;
-
-        for (int i = size - 1; i > insertIndex; i--) {
-            storage[i] = storage[i - 1];
-        }
+        System.arraycopy(storage, insertIndex, storage, insertIndex + 1, size - insertIndex);
         storage[insertIndex] = resume;
+        size++;
     }
 
     @Override
     protected void deleteByIndex(int index) {
-        while (index < size - 1) {
-            storage[index] = storage[index + 1];
-            storage[index + 1] = null;
-            index++;
-        }
+        System.arraycopy(storage, index + 1, storage, index, size - (index + 1));
         size--;
     }
 }
