0265176dd32088762f3db96808b3129d55305624
Max Koshelev <maxdiorio@yandex.ru>
2020-02-01 12:01:18 +0300

    HW12 (Srtream API tasks)

--- src/com/maxkosh/webapp/StreamApiMain.java
+++ src/com/maxkosh/webapp/StreamApiMain.java
@@ -1,22 +1,35 @@
 package com.maxkosh.webapp;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-import static java.lang.Integer.parseInt;
-
 public class StreamApiMain {
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3 , 3 , 2, 3};
+        //Java 8 Streams I:
+        int[] numbers = {9, 8, 7, 4, 2, 2};
         System.out.println(minValue(numbers));
+
+        //Java 8 Streams II:
+        List<Integer> integers = new ArrayList<>();
+        for (int i = 2; i < 10; i++) {
+            integers.add(i);
+        }
+        System.out.println(oddOrEven(integers));
     }
 
     static int minValue(int[] values) {
-        IntStream stream = Arrays.stream(values);
-        //stream.map(i -> i * 2).forEach(System.out::println);
+        final int[] count = {((int) IntStream.of(values).distinct().count() - 1)};
+        return IntStream.of(values).distinct().sorted()
+                .map(i -> i * (int) Math.pow(10, (count[0]--))).sum();
+    }
 
-        //Stream.iterate(values, );
-        return stream.sorted().distinct().sum();
+    static List<Integer> oddOrEven(List<Integer> integers) {
+        if (integers.stream().mapToInt(Integer::intValue).sum() % 2 == 0) {
+            return integers.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
+        }
+        return integers.stream().filter(x -> x % 2 != 0).collect(Collectors.toList());
     }
 }
